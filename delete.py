f09837fc599bdc6625e2c4655872ba14541146ff
Andrew Mahran <mahranandrew@gmail.com>
2025-08-19 16:03:31 -0700

    removed unnecessary scripts

--- delete.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", 11123))
-
-print("Listening for GPS data on port 11123...")
-while True:
-    data, addr = sock.recvfrom(1024)
-    print("From phone:", data.decode().strip())
